63dd9de68018e195d4b1b5065275d26d46425f24
Alejandro Moreno <alejandrogermaine@gmail.com>
2026-03-21 22:43:07 +0100

    Rediseño login y coherencia visual entre paneles.

--- GrimorioApp/src/app/login/login.ts
+++ GrimorioApp/src/app/login/login.ts
@@ -13,25 +13,29 @@ import { LoginDTO } from '../dtos/LoginDTO';
 })
 export class Login {
   constructor (
-    private usuarioService : UsuarioService, 
+    private usuarioService : UsuarioService,
     private router : Router
   ){ }
-  
+
 
   public login : LoginDTO = {
     Correo : "admin@admin.com",
     Clave : "admin"
   }
 
-  iniciarSesion(){
-    this.usuarioService.iniciarSesion(this.login).subscribe({next : (res) => {
-      console.log(res)
+public errorMensaje: string = '';
 
+  iniciarSesion(){
+    this.errorMensaje = '';
+    this.usuarioService.iniciarSesion(this.login).subscribe({
+      next : (res) => {
       if(res.status){
         this.router.navigate(["pages/dashboard"])
+      } else {
+        this.errorMensaje = res.msg || 'Credenciales incorrectas';
       }
-      
     }, error : (err) => {
+      this.errorMensaje = 'Error al conectar con el servidor';
       console.log(err)
     }})
   }
